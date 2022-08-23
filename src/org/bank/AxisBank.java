4cc9974fe8dcc38d3b69ae67429be045f566a52c
priya0010 <priyaece4d@gmail.com>
2022-08-23 12:57:09 +0530

    message

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class AxisBank extends BankInfo{
+public void deposit() {
+	
+System.out.println("3%");
+}
+public static void main(String[] args) {
+	AxisBank a=new AxisBank();
+	a.deposit();
+	a.fixed();
+	a.saving();
+	
+}
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+public void saving() {
+System.out.println("5%");
+
+}
+public void fixed() {
+	
+System.out.println("3%");
+}
+public void deposit() {
+	
+System.out.println("2%");
+}
+}
